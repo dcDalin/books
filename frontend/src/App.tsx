11b33f1c1df9035d7e25ccd914758b92bca57e3c
dcDalin <30606041+dcDalin@users.noreply.github.com>
2024-06-11 04:34:06 +0300

    setup draft apollo client

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,13 @@
+import { ApolloProvider } from '@apollo/client';
+
+import { client } from './utils/client';
+
 function App() {
-  return <h1>hello</h1>;
+  return (
+    <ApolloProvider client={client}>
+      <h1>hello</h1>
+    </ApolloProvider>
+  );
 }
 
 export default App;
