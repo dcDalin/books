401d678f079a1b55fdd90ae9b8fbef761426b381
dcDalin <30606041+dcDalin@users.noreply.github.com>
2024-06-11 04:28:53 +0300

    install material ui and mullish font face

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,3 +1,7 @@
+import '@fontsource/mulish';
+import '@fontsource/mulish/400.css';
+import '@fontsource/mulish/400-italic.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
