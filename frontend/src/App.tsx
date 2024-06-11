a388c5e76d8090c97faa083e54bed9df8159fd6f
dcDalin <30606041+dcDalin@users.noreply.github.com>
2024-06-11 04:38:02 +0300

    add react router and draft pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,32 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import { ApolloProvider } from '@apollo/client';
 
+import MainLayout from './layouts/main-layout';
+import ErrorPage from './pages/error-page';
 import { client } from './utils/client';
+
+const router = createBrowserRouter([
+  {
+    element: <MainLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/',
+        element: <div>Home Screen</div>,
+      },
+      {
+        path: '/book',
+        element: <div>Single book</div>,
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
     <ApolloProvider client={client}>
-      <h1>hello</h1>
+      <RouterProvider router={router} />
     </ApolloProvider>
   );
 }
